1c4e4f7cf19220579090c6d8e7054db358fe85f3
Ricardo Busquet <7198302+rbusquet@users.noreply.github.com>
2021-04-09 14:27:50 -0400

    404

---
+++ pages/404.tsx
@@ -0,0 +1,10 @@
+import type { NextPage } from "next/types"
+import Layout from "../components/layout"
+
+const FourOhFour: NextPage = () => (
+  <Layout>
+    <h2>404: This page could not be found.</h2>
+  </Layout>
+)
+
+export default FourOhFour
