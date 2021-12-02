3de79f4a527f432a18da20cd59b87f0290c391d7
Ricardo Busquet <7198302+rbusquet@users.noreply.github.com>
2021-12-02 08:16:08 -0500

    links and toc

--- remix.config.js
+++ remix.config.js
@@ -7,15 +7,25 @@ module.exports = {
   publicPath: "/build/",
   serverBuildDirectory: "api/build",
   mdx: async (filename) => {
-    const [rehypeHighlight, remarkGfm, remarkGemoji] = await Promise.all([
+    const [
+      rehypeHighlight,
+      rehypeSlug,
+      rehypeAutoLink,
+      remarkToc,
+      remarkGfm,
+      remarkGemoji,
+    ] = await Promise.all([
       import("rehype-highlight").then((mod) => mod.default),
+      import("rehype-slug").then((mod) => mod.default),
+      import("rehype-autolink-headings").then((mod) => mod.default),
+      import("remark-toc").then((mod) => mod.default),
       import("remark-gfm").then((mod) => mod.default),
       import("remark-gemoji").then((mod) => mod.default),
     ]);
 
     return {
-      rehypePlugins: [rehypeHighlight],
-      remarkPlugins: [remarkGfm, remarkGemoji],
+      rehypePlugins: [rehypeHighlight, rehypeSlug, rehypeAutoLink],
+      remarkPlugins: [remarkToc, remarkGfm, remarkGemoji],
     };
   },
 };
