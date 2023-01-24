c07cbfd55c5194ad41e6b35a52aacc1bbc1e69a3
Ricardo Busquet <7198302+rbusquet@users.noreply.github.com>
2023-01-24 18:02:17 -0500

    convert to astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,18 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
+
+import slug from "rehype-slug";
+import autolink from "rehype-autolink-headings";
+import toc from "remark-toc";
+import gfm from "remark-gfm";
+import gemoji from "remark-gemoji";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  markdown: {
+    remarkPlugins: [toc, gfm, gemoji],
+    rehypePlugins: [slug, autolink],
+    shikiConfig: {
+      theme: "light-plus",
+    },
+  },
+});
