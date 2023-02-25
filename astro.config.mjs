552981444db7820e8e0323f323d103dc071bbdb5
Ricardo Busquet <7198302+rbusquet@users.noreply.github.com>
2023-02-24 23:42:49 -0500

    Vercel edge astro (#17)
    * try vercel edge
    
    * add analytics
    
    * remove get static paths
    
    * redirect to 404
    
    * uses use ssr
    
    * default to en

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,12 +5,28 @@ import gfm from "remark-gfm";
 import emoji from "remark-emoji";
 
 // https://astro.build/config
+import vercel from "@astrojs/vercel/edge";
+
+// https://astro.build/config
 export default defineConfig({
   markdown: {
-    remarkPlugins: [[emoji, { accessible: true }], toc, gfm],
+    remarkPlugins: [
+      [
+        emoji,
+        {
+          accessible: true,
+        },
+      ],
+      toc,
+      gfm,
+    ],
     rehypePlugins: [slug],
     shikiConfig: {
       theme: "light-plus",
     },
   },
+  output: "server",
+  adapter: vercel({
+    analytics: true,
+  }),
 });
